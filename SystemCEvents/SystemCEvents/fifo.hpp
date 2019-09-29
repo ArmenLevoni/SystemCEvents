4d5a3105162bc6f35c09e58fcdb7906402d8bb31
Armen <armkha01@arm.com>
2019-09-29 20:48:31 +0100

    Initial test

---
+++ SystemCEvents/SystemCEvents/fifo.hpp
@@ -0,0 +1,14 @@
+//
+//  fifo.hpp
+//  SystemCEvents
+//
+//  Created by Armen Khachatryan on 29/09/2019.
+//  Copyright © 2019 Armen Khachatryan. All rights reserved.
+//
+
+#ifndef fifo_hpp
+#define fifo_hpp
+
+#include <stdio.h>
+
+#endif /* fifo_hpp */
---
+++ SystemCEvents/SystemCEvents/main.cpp
@@ -0,0 +1,7 @@
+#include <systemc>
+
+int sc_main_1(int argc, char *argv[]) {
+    std::cout << "Hello, World!\n";
+    sc_core::sc_start();
+    return 0;
+}
